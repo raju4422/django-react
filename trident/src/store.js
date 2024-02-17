689e1552e7bdc0df681e9b3f63af8bbae410f3db
Raju <67702804+raju4422@users.noreply.github.com>
2024-02-17 12:34:43 +0530

    updated store

--- trident/src/store.js
+++ trident/src/store.js
@@ -1,15 +1,27 @@
 
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore} from 'redux-persist';
-
-// import { composeWithDevTools } from '@reduxjs/toolkit/dist/devtoolsExtension';
-// import thunk from 'redux-thunk';
+// import { persistStore} from 'redux-persist';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'
 import persistedReducer from './reducers/rootReducer';
 
 
 // const middleware = [thunk]
 
-const store = configureStore({reducer:persistedReducer});
+const store = configureStore({reducer:persistedReducer,middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),});
 export const persistor = persistStore(store);
 
 export default store;
